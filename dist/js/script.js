9fa0263fbcb7d571b783f5040bb2fae5731ea136
AlexFili <RootyRoots1507@gmail.com>
2024-01-18 14:59:55 +0300

    validation is ready

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,7 +3,7 @@
 window.addEventListener('DOMContentLoaded', () => {
     let 
         advantagesInput = document.querySelectorAll('.advantages-input'),
-        removeButtons = document.querySelectorAll('.third-screen__remove-btn'),
+        removeButtons = document.querySelectorAll('.third-screen__remove-btn2'),
         plus = document.querySelector('.third-screen__form__add-btn');
 
     const 
@@ -31,6 +31,19 @@ window.addEventListener('DOMContentLoaded', () => {
         modalSuccess = document.querySelector('.modal-success'),
         blackout = document.querySelector('.blackout'),
         buttonToMain = document.getElementById('button-to-main'),
+        inputText = document.querySelectorAll('[type="text"]'),
+        inputMail = document.querySelector('[type="email"]'),
+        firstForm = document.querySelectorAll('[data-num-page="1"]'),
+        errorlabel = document.querySelectorAll('.errorlabel'),
+        inputPagetwo = document.querySelectorAll('.second-screen__form_input'),
+        advantagesVisibility = document.querySelectorAll('[data-visibility="visible"]'),
+        checkboxGroup = document.querySelectorAll('[type="checkbox"]'),     
+        checkboxP = document.getElementById('checkbox-p'),  
+        textarea = document.getElementById('field-about'),
+        textareaTip = document.querySelector('.four-screen__tip'),
+        parentPlus = document.querySelector('.third-screen__form'),
+        allInputs = document.getElementsByTagName('input'),
+
         showSexList = () => {            
             ChioseBlock.classList.toggle('changeSexList');
             svgSex.classList.toggle('rotate-180');
@@ -40,9 +53,19 @@ window.addEventListener('DOMContentLoaded', () => {
             navBtns.classList.toggle('widthMinus20px');
             stepsLineGrey.classList.toggle('left24px');                                     
         },
-        recalculation = () => {
-            advantagesInput = document.querySelectorAll('.advantages-input');
-            removeButtons = document.querySelectorAll('.third-screen__remove-btn');
+        removeError = (inputList) => {
+            inputList.forEach(input => {
+                if (input.classList.contains('errorBorder')) {
+                    input.classList.remove('errorBorder'); 
+                }
+                if (input.previousSibling.textContent && clickOnNext !== 0) {
+                    input.previousSibling.textContent = '';
+                }   
+                if (checkboxP.textContent = 'Not selected') {
+                    checkboxP.textContent = 'Checkbox group';
+                    checkboxP.style.color = 'black';
+                }                
+            });           
         };
 
     //навигация по страницам
@@ -51,16 +74,150 @@ window.addEventListener('DOMContentLoaded', () => {
     let offsetProgressLine = 0;  
     let offsetCircleMove = 0; 
     let clickOnNext = -1;
+    //количество полей ввода
+    let InputCount = 1;
+    //актуальный массив из id видимых полей
+    let visibleInputs = [];
+    //индекс для visibleInputs
+    let f = 0;
+    //количество пустых чекбоксов
+    let checkboxEmptyCounter = 0;
 
     //к началу формы
     start.addEventListener('click', () => {
-        offsetStart += 900;        
+    //валидация формы 1-й страницы
+        removeError(firstForm);
+        let result = true;
+        const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+        firstForm.forEach(input => {
+            //на пустое поле
+            if (input.value == '') {
+                input.classList.add('errorBorder');
+                input.previousSibling.textContent = "You haven't entered anything";
+                result = false;
+            }
+            //на 11 цифр
+            else if (input.value.length && input.type == 'number')  {                
+                if (input.value.length !== 11) {
+                    input.classList.add('errorBorder');
+                    input.previousSibling.textContent = 'Enter 11 numbers in the format "79998887766"';
+                    result = false;
+                }
+            }
+            // на корректную почту
+            else if (input.type == 'email' && EMAIL_REGEXP.test(inputMail.value) == false || inputMail.value.length > 30) {
+                
+                
+                    inputMail.classList.add('errorBorder');
+                    inputMail.previousSibling.textContent = 'Enter your valid email address';
+                    result = false;                
+            };
+            console.log(inputMail.value.length > 30);
+            console.log(input.type == 'email');
+            console.log(EMAIL_REGEXP.test(inputMail.value) == false);
+        });  
+        if (result == true) {
+            offsetStart += 900;        
         startNslider.style.transform = `translateX(-${offsetStart}px)`;
+        };
+    });  
+    //помещаем выбор пользователя в INPUT 
+    sexListItems.forEach(item => {
+        item.addEventListener('click', () => {            
+            selectSex.value = item.dataset.value;  
+            console.log(selectSex.value);         
+        });
     });
-
     //листаем форму вперед
-    next.addEventListener('click', () => {
-        if (offsetSlide, offsetProgressLine, offsetCircleMove >= 0 && offsetSlide, offsetProgressLine, offsetCircleMove < 500) {
+    
+    next.addEventListener('click', () => {        
+        let result = true;
+        if (clickOnNext == -1) {
+            inputPagetwo.forEach(input => {
+                if (input.nextSibling.classList.contains('errorTip')) {
+                    input.nextSibling.classList.remove('errorTip'); 
+                    input.nextSibling.textContent = 'Tip';                   
+                }
+                if (input.classList.contains('errorBorder')) {
+                    input.classList.remove('errorBorder');                    
+                }
+            });
+        };
+        removeError(inputPagetwo);
+        //валидация формы 2-й страницы        
+        if (clickOnNext == -1) {             
+            inputPagetwo.forEach(input => {                               
+                if (input.value == '') {
+                    input.nextSibling.textContent = "The field must be filled in";
+                    input.nextSibling.classList.add('errorTip');
+                    result = false;
+                }
+                if (input.value) {
+                    if (input.value.length < 2) {
+                        input.classList.add('errorBorder');
+                        input.nextSibling.textContent = "Enter at least 2 characters";
+                        input.nextSibling.classList.add('errorTip');
+                        result = false;
+                    }
+                    if (input.value.length > 30) {
+                        input.classList.add('errorBorder');
+                        input.nextSibling.textContent = "Maximum of 30 characters";
+                        result = false;
+                    }                                   
+                };
+                
+            });           
+        };
+        //валидация формы 3-й страницы
+        //пресчитываем видимых полей при любом взамиодействии с third-screen__form
+        parentPlus.addEventListener('click', () => {   
+            f = 0;    
+            for (let i = 0; i < parentPlus.firstElementChild.children.length; i++) {            
+                if (parentPlus.firstElementChild.children[i].dataset.visibility == 'visible') {
+                    visibleInputs[f] = parentPlus.firstElementChild.children[i].getAttribute('id');  
+                    f++;              
+                };           
+            };
+            console.log(visibleInputs);
+            console.log(`количество в массиве видимых (f) = ${f}`);            
+        });        
+        if (clickOnNext == 0 ) {          
+             
+            //динамические поля
+            removeError(advantagesInput)
+            advantagesInput.forEach((input, i) => {                
+                if (input.getAttribute('id') == visibleInputs[i]) {
+                    //если пустое поле, то красная рамка и просьба заполнить поле
+                    if (input.value == '') { 
+                        input.classList.add('errorBorder');
+                        input.classList.add('errorPlaceholder')
+                        input.placeholder = 'Fill out this';                   
+                        result = false;       
+                    } 
+                    //если меньше 2-х символов, то красная рамка и предупреждение
+                    if (input.value && input.value.length < 2) { 
+                        input.value = "";             
+                        input.classList.add('errorBorder');
+                        input.placeholder = "minimum of 2 characters"
+                        input.classList.add('errorPlaceholder')
+                        result = false;                    
+                    }
+                }                
+            });         
+            //чекбокс
+            checkboxGroup.forEach(item => {
+                if (item.checked == false) {
+                    checkboxEmptyCounter++;                   
+                }                              
+            });
+            if (checkboxEmptyCounter == 3) {
+                checkboxP.textContent = 'Not selected';
+                checkboxP.style.color = 'red';
+                result = false;
+            }
+        }        
+        //перелистывание
+        if (offsetSlide, offsetProgressLine, offsetCircleMove >= 0 && offsetSlide, offsetProgressLine, offsetCircleMove < 500 && result == true) {
             offsetSlide += 680;
             offsetProgressLine += 337;
             offsetCircleMove += 332;
@@ -71,7 +228,6 @@ window.addEventListener('DOMContentLoaded', () => {
             circleMove.style.transform = `translateX(${offsetCircleMove}px)`;      
             circleGrey[clickOnNext].style.display = 'none';
             circleDone[clickOnNext].style.display = 'block';
-            console.log(clickOnNext);
             if (clickOnNext === 1) {
                 windowSmall.style.height = '500px';
                 navBtns.style.bottom = '283px';
@@ -79,9 +235,8 @@ window.addEventListener('DOMContentLoaded', () => {
                 buttonSend.style.display = 'block';
             };
         };
-        
+        console.log(clickOnNext);
     }); 
-
     //листаем форму назад
     prev.addEventListener('click', () => {
         if (offsetSlide, offsetProgressLine, offsetCircleMove >= 0 && clickOnNext >= 0) {
@@ -101,17 +256,36 @@ window.addEventListener('DOMContentLoaded', () => {
                 buttonSend.style.display = 'none';
             };           
         };
+        console.log(clickOnNext);
     });
     //отправить форму
-    buttonSend.addEventListener('click', () => {        
-        blackout.classList.toggle('show');
-        modalSuccess.style.visibility = 'visible';
-        modalSuccess.style.opacity = '1';
-    });
-    //перезагрузка после кнопки (на главную страницу)
-    buttonToMain.addEventListener('click', () => {
+    buttonSend.addEventListener('click', (e) => { 
+        e.preventDefault();
+        if (textarea.value) {
+            textareaTip.textContent = 'Tip';
+            textareaTip.style.color = 'black';
+        }
+        let result = true; 
+        console.log(textareaTip);
+        if (textarea.value == '' || textarea.value.length < 2) {
+            console.log('err')
+            textareaTip.textContent = 'Tell us about yourself in a free form';
+            textareaTip.style.color = 'red';
+            result = false;  
+            return result;          
+        }      
+        if (result == true) {
+            console.log('test')
+            blackout.classList.toggle('show');
+            modalSuccess.style.visibility = 'visible';
+            modalSuccess.style.opacity = '1';
+        }
+        //перезагрузка после кнопки (на главную страницу)
+        buttonToMain.addEventListener('click', () => {
         location.reload();
+        });
     });
+    
 
     //выдвигаем меню выбора пола
     selectSex.addEventListener('click', showSexList);
@@ -132,45 +306,60 @@ window.addEventListener('DOMContentLoaded', () => {
             stepsLineGrey.classList.toggle('left24px');                                 
         });
     });   
-    
-    //наблюдение за изменениями DOM-дерева
-    const mutationObserver = new MutationObserver(recalculation => {
-        advantagesInput = document.querySelectorAll('.advantages-input');
-        removeButtons = document.querySelectorAll('.third-screen__remove-btn');
-        console.log(recalculation);
-    });
-    mutationObserver.observe(advantagesInputBlock, {childList: true});
-    mutationObserver.observe(buttonsBlock, {childList: true});
-
+    //создаем массив для удаленных полей
+    const removeInputs = [];
+    //счетчик удаленных полей
+    let j = 0;
     //добавляем поля ввода
     plus.addEventListener('click', () => {
-        const newInput = document.createElement('input');
-        newInput.classList.add('advantages-input');
-        newInput.setAttribute('id', 'field-advantages-4');
-        newInput.setAttribute('name', 'advantages');
-        newInput.setAttribute('placeholder', 'Placeholder');
-        newInput.setAttribute('type', 'text');        
-        advantagesInputBlock.appendChild(newInput);
-
-        const newRemove = document.createElement('div');
-        newRemove.classList.add('third-screen__remove-btn');
-        newRemove.setAttribute('id', 'button-remove-4');
-        newRemove.innerHTML = '<img src="icons/remove.svg" alt="remove">';
-        buttonsBlock.appendChild(newRemove);
-
-        recalculation();
-    });   
-    
-
-    //удаляем лишние поля ввода     
-    removeButtons.forEach((itemRemove, i) => {
-        recalculation();
-        itemRemove.addEventListener('click', () => {
-            itemRemove.remove();
-            advantagesInput[i].remove();
-            recalculation();
-            console.log(advantagesInput.length); 
+        //плюс исчезает, если полей 5шт
+        if (InputCount >= 4) {
+            plus.style.cssText = 'display: none';
+        }
+        //достаем из массива удаленные поля
+        if (j > 0) {
+            advantagesInput[removeInputs[j-1]].style.cssText = 'display: block';
+            removeButtons[removeInputs[j-1]].style.cssText = 'display: block';
+            advantagesInput[removeInputs[j-1]].dataset.visibility = 'visible'
+            removeInputs.pop()
+            j--;
+            InputCount++;
+        }
+        else if (InputCount < 4) {
+            //если массив удаленных полей пуст, то показываем новые
+            advantagesInput[InputCount + 1].style.cssText = 'display: block';
+            removeButtons[InputCount + 1].style.cssText = 'display: block';
+            advantagesInput[InputCount + 1].dataset.visibility = 'visible'
+            InputCount++;         
+        };       
+    }); 
+    removeButtons.forEach((removeItem, i) => {         
+        removeItem.addEventListener('click', () => {
+            if (InputCount > 0) {
+                advantagesInput[i].value = '';
+                advantagesInput[i].placeholder = "Placeholder";
+                advantagesInput[i].classList.remove('errorBorder');
+                advantagesInput[i].classList.remove('errorPlaceholder');
+                advantagesInput[i].dataset.visibility = 'hidden';
+                advantagesInput[i].style.cssText = 'display: none';
+                removeButtons[i].style.cssText = 'display: none';
+                InputCount--;  
+                removeInputs[j] = i;
+                j++;
+                for (let h = 0; h <= visibleInputs.length; h++) {
+                    if (visibleInputs[h] == removeItem.dataset.removeinput) {
+                        console.log(`удаляемый элемент ${removeItem.dataset.removeinput}`);
+                        console.log(`индекс в массиве видимых (h) =${h}` );
+                        visibleInputs.splice(h, 2);
+                    }
+                }               
+                            
+            }  
+            if (InputCount < 4) {
+                plus.style.cssText = 'display: block';
+            }              
         });
-            
     });
+
 });
+
